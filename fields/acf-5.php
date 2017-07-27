fb677217c8df963b139f381d6c76d6ca01f1567b
Maxime Culea <culea.maxime@gmail.com>
2017-07-27 16:29:53 +0200

    Add custom classes for 5 and 5.6 field

---
+++ fields/acf-5.php
@@ -0,0 +1,29 @@
+<?php class acf_field_svg_icon_5 extends acf_field_svg_icon {
+
+	function __construct() {
+		// do not delete!
+		parent::__construct();
+	}
+
+	/**
+	 * Enqueue assets for the SVG icon field in admin
+	 *
+	 * @since 1.0.0
+	 */
+	function input_admin_enqueue_scripts() {
+		// The suffix.
+		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG === true ? '' : '.min';
+
+		// Scripts.
+		wp_register_script( 'acf-input-svg-icon', ACF_SVG_ICON_URL . 'assets/js/input-5' . $suffix . '.js', array(
+			'jquery',
+			'select2',
+			'acf-input'
+		), ACF_SVG_ICON_VER );
+
+		// Enqueuing.
+		wp_enqueue_script( 'acf-input-svg-icon' );
+
+		parent::input_admin_enqueue_scripts();
+	}
+}
